84743a6bc507122a179435280c83417e7577030b
paul <paul.gardien@csgroup.eu>
2022-07-13 13:59:53 +0200

    add sources

---
+++ Extensions/Extensions/list.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public class List
+    {
+        public static readonly string[] executabe =
+        {
+            ".apk",
+            ".bat",
+            ".bin",
+            ".cgi",
+            ".com",
+            ".exe",
+            ".jar",
+            ".py",
+            ".wsf"
+        };
+
+        public static readonly string[] audio =
+        {
+            ".aif",
+            ".cda",
+            ".iff",
+            ".mid",
+            ".midi",
+            ".mp3",
+            ".mpa",
+            ".wav",
+            ".wma",
+            ".wpl"
+        };
+
+        public static readonly string[] image =
+        {
+            ".ai",
+            ".bmp",
+            ".ico",
+            ".gif",
+            ".jpeg",
+            ".jpg",
+            ".max",
+            ".obj",
+            ".png",
+            ".ps",
+            ".psd",
+            ".svg",
+            ".tif",
+            ".tiff",
+            ".3ds",
+            ".3dm"
+        };
+
+        public static readonly string[] video =
+        {
+            ".avi",
+            ".flv",
+            ".h264",
+            ".m4v",
+            ".mkv",
+            ".mov",
+            ".mp4",
+            ".mpg",
+            ".mpeg",
+            ".rm",
+            ".swf",
+            ".vob",
+            ".wmv",
+            ".3g2",
+            ".3gp"
+        };
+
+        public static readonly string[] document =
+        {
+            ".doc",
+            ".docx",
+            ".odt",
+            ".msg",
+            ".pdf",
+            ".rtf",
+            ".tex",
+            ".txt",
+            ".wks",
+            ".wps",
+            ".wpd",
+            ".ods",
+            ".xlr",
+            ".xls",
+            ".xlsx",
+            ".key",
+            ".odp",
+            ".pps",
+            ".ppt",
+            ".pptx"
+        };
+
+        public static readonly string[] database =
+        {
+            ".accdb",
+            ".csv",
+            ".dat",
+            ".db",
+            ".dbf",
+            ".log",
+            ".mdb",
+            ".pdb",
+            ".sav",
+            ".sql"
+        };
+
+        public static readonly string[] web =
+        {
+            ".asp",
+            ".aspx",
+            ".cer",
+            ".cfm",
+            ".cgi",
+            ".pl",
+            ".htm",
+            ".html",
+            ".js",
+            ".jsp",
+            ".part",
+            ".php",
+            ".rss",
+            ".xhtml"
+        };
+    }
+}
